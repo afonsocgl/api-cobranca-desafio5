1b4fd55f98678724c985980a4e9a7f965da01a89
afonsocgl <afonsocgl@gmail.com>
2021-10-06 23:45:50 -0300

    Alterado DB da conexão

--- conexao.js
+++ conexao.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'market_cubos',
+    database: 'api_cobranca',
     password: 'Af920204',
     port: 5432
 });

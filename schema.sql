03215a05b478a43582cbc1804fa15b8f66411227
afonsocgl <afonsocgl@gmail.com>
2021-10-07 23:07:57 -0300

    Correção do schema

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE usuarios(
     id serial primary key,
     nome varchar(50) not null,
     email varchar(60) not null,
-    senha varchar(15) not null
+    senha varchar(15) not null,
     cpf varchar(14),
     telefone varchar(14)
 );

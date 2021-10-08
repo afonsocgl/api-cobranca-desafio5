5e3d927addd8c3e70d186c28d68d34e730a05f24
afonsocgl <afonsocgl@gmail.com>
2021-10-07 22:41:37 -0300

    Criação do schema da tabela de Clientes para BD

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE api_cobranca;
 
-DROP TABLE IF EXISTS usuarios:
+DROP TABLE IF EXISTS usuarios;
 CREATE TABLE usuarios(
     id serial primary key,
     nome varchar(50) not null,
@@ -10,3 +10,18 @@ CREATE TABLE usuarios(
     telefone varchar(14)
 );
 
+
+DROP TABLE IF EXISTS clientes;
+CREATE TABLE clientes(
+    id serial primary key,
+    nome varchar(50) not null,
+    email varchar(60) not null,
+    cpf varchar(14) not null,
+    telefone varchar(14) not null,
+    cep varchar(10),
+    logradouro varchar(100),
+    complemento varchar(30),
+    bairro varchar(50),
+    cidade varchar(50),
+    estado varchar(20)
+);

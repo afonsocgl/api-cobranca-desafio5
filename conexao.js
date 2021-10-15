e5ccf2fc0a5de641c9598e807c40c934f083c610
afonsocgl <afonsocgl@gmail.com>
2021-10-14 21:22:39 -0300

    Retornando conexão do BD para localhost

--- conexao.js
+++ conexao.js
@@ -1,14 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'istapowdfadyft',
-    host: 'ec2-54-166-120-40.compute-1.amazonaws.com',
-    database: 'dau1h546gi3dnq',
-    password: 'cd27e349fd1fc1747d3946bb4b9a3bd630ed7fb25c7a75c9600b6ff82b657b51',
-    port: 5432,
-    ssl:{
-        rejectUnauthorized: false
-    }
+    user: 'postgres',
+    host: 'localhost',
+    database: 'api_cobranca',
+    password: 'Af920204',
+    port: 5432
 });
 
 const query = (text, param) =>{
@@ -18,5 +15,3 @@ const query = (text, param) =>{
 module.exports ={
     query
 };
-
-

ba47cff73be1617cc9e7982c21a5ba7c512b2968
afonsocgl <afonsocgl@gmail.com>
2021-10-14 21:40:18 -0300

    ALterando conexão para BD da Heroku

--- conexao.js
+++ conexao.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'api_cobranca',
-    password: 'Af920204',
-    port: 5432
+    user: 'istapowdfadyft',
+    host: 'ec2-54-166-120-40.compute-1.amazonaws.com',
+    database: 'dau1h546gi3dnq',
+    password: 'cd27e349fd1fc1747d3946bb4b9a3bd630ed7fb25c7a75c9600b6ff82b657b51',
+    port: 5432,
+    ssl:{
+        rejectUnauthorized: false
+    }
 });
 
 const query = (text, param) =>{
@@ -15,3 +18,20 @@ const query = (text, param) =>{
 module.exports ={
     query
 };
+
+
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'api_cobranca',
+//     password: 'Af920204',
+//     port: 5432
+// });
+
+// const query = (text, param) =>{
+//     return pool.query(text, param);
+// };
+
+// module.exports ={
+//     query
+// };
